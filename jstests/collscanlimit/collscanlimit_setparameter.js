39942308c43fca1d3d2e51939842592ffaf52226
Igor Solodovnikov <igor.solodovnikov@percona.com>
2017-08-11 18:57:02 +0300

    PSMDB-134 post-review changes part 2

--- jstests/collscanlimit/collscanlimit_setparameter.js
+++ jstests/collscanlimit/collscanlimit_setparameter.js
@@ -14,15 +14,17 @@
         assert.eq(res.profilingCollScanLimit, 1000);
 
         // check setting boolean true
-        db.getSiblingDB('admin').runCommand( { setParameter: 1, "profilingCollScanLimit": true } );
+        res = db.getSiblingDB('admin').runCommand( { setParameter: 1, "profilingCollScanLimit": true } );
         assert.commandWorked(res, "setParameter failed to set profilingCollScanLimit");
+        assert.eq(res.was, 1000);
         res = db.getSiblingDB('admin').runCommand( { getParameter: 1, "profilingCollScanLimit": 1 } );
         assert.commandWorked(res, "getParameter failed to get profilingCollScanLimit");
         assert.eq(res.profilingCollScanLimit, 1);
 
         // check setting boolean false
-        db.getSiblingDB('admin').runCommand( { setParameter: 1, "profilingCollScanLimit": false } );
+        res = db.getSiblingDB('admin').runCommand( { setParameter: 1, "profilingCollScanLimit": false } );
         assert.commandWorked(res, "setParameter failed to set profilingCollScanLimit");
+        assert.eq(res.was, 1);
         res = db.getSiblingDB('admin').runCommand( { getParameter: 1, "profilingCollScanLimit": 1 } );
         assert.commandWorked(res, "getParameter failed to get profilingCollScanLimit");
         assert.eq(res.profilingCollScanLimit, 0);
