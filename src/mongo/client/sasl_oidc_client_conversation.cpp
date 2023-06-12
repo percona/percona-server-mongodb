c0e883719a22a715fedc721b34edab26a2d2ea18
Adrian Gonzalez <adriangonzalezmontemayor@gmail.com>
2023-06-12 15:35:05 +0000

    SERVER-77839 Extend OIDC testing coverage to other IDPs
    (cherry picked from commit 27bda12dc71fb1dbca0252d9ae8fdafebb51aa9c)
    
    Conflicts:
        jstests/auth/lib/automated_idp_authn_simulator_okta.py

--- src/mongo/client/sasl_oidc_client_conversation.cpp
+++ src/mongo/client/sasl_oidc_client_conversation.cpp
@@ -51,6 +51,7 @@ constexpr auto kGrantTypeParameterDeviceCodeValue =
     "urn:ietf:params:oauth:grant-type:device_code"_sd;
 constexpr auto kGrantTypeParameterRefreshTokenValue = "refresh_token"_sd;
 constexpr auto kDeviceCodeParameterName = "device_code"_sd;
+constexpr auto kCodeParameterName = "code"_sd;
 constexpr auto kRefreshTokenParameterName = kGrantTypeParameterRefreshTokenValue;
 
 std::unique_ptr<HttpClient> createHttpClient(bool withConnectionPool = false) {
@@ -81,8 +82,10 @@ inline void appendPostBodyDeviceCodeRequestParams(
 }
 
 inline void appendPostBodyTokenRequestParams(StringBuilder* sb, StringData deviceCode) {
+    // kDeviceCodeParameterName and kCodeParameterName are the same, IDP's use different names.
     *sb << "&" << kGrantTypeParameterName << "=" << kGrantTypeParameterDeviceCodeValue << "&"
-        << kDeviceCodeParameterName << "=" << uriEncode(deviceCode);
+        << kDeviceCodeParameterName << "=" << uriEncode(deviceCode) << "&" << kCodeParameterName
+        << "=" << uriEncode(deviceCode);
 }
 
 inline void appendPostBodyRefreshFlowParams(StringBuilder* sb, StringData refreshToken) {
@@ -136,8 +139,18 @@ std::pair<std::string, std::string> doDeviceAuthorizationGrantFlow(
     // Simulate end user login via user verification URI.
     auto deviceAuthorizationResponse = OIDCDeviceAuthorizationResponse::parse(
         IDLParserContext{"oidcDeviceAuthorizationResponse"}, deviceAuthorizationResponseObj);
+
+    // IDP's use different names to refer to the verification url.
+    const auto& optURI = deviceAuthorizationResponse.getVerificationUri();
+    const auto& optURL = deviceAuthorizationResponse.getVerificationUrl();
+    uassert(ErrorCodes::BadValue, "Encountered empty device authorization url", optURI || optURL);
+    uassert(ErrorCodes::BadValue,
+            "Encounterd both verification_uri and verification_url",
+            !(optURI && optURL));
+    auto deviceAuthURL = optURI ? optURI.get() : optURL.get();
+
     oidcClientGlobalParams.oidcIdPAuthCallback(
-        principalName, deviceAuthorizationResponse.getVerificationUriComplete());
+        principalName, deviceAuthURL, deviceAuthorizationResponse.getUserCode());
 
     // Poll token endpoint for access and refresh tokens. It should return immediately since
     // the shell blocks on the authenticationSimulator until it completes, but poll anyway.
