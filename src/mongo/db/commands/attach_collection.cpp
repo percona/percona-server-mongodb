47001e0ef641133258fadbffaa7cd3aa05f4f5c1
Igor Solodovnikov <igor.solodovnikov@percona.com>
2023-11-20 14:32:12 +0000

    PSMDB-1284 Create attachCollection command

---
+++ src/mongo/db/commands/attach_collection.cpp
@@ -0,0 +1,103 @@
+/*======
+This file is part of Percona Server for MongoDB.
+
+Copyright (C) 2023-present Percona and/or its affiliates. All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Server Side Public License, version 1,
+    as published by MongoDB, Inc.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Server Side Public License for more details.
+
+    You should have received a copy of the Server Side Public License
+    along with this program. If not, see
+    <http://www.mongodb.com/licensing/server-side-public-license>.
+
+    As a special exception, the copyright holders give permission to link the
+    code of portions of this program with the OpenSSL library under certain
+    conditions as described in each individual source file and distribute
+    linked combinations including the program with the OpenSSL library. You
+    must comply with the Server Side Public License in all respects for
+    all of the code used other than as permitted herein. If you modify file(s)
+    with this exception, you may extend this exception to your version of the
+    file(s), but you are not obligated to do so. If you do not wish to do so,
+    delete this exception statement from your version. If you delete this
+    exception statement from all source files in the program, then also delete
+    it in the license file.
+======= */
+
+
+#include "mongo/db/auth/authorization_session.h"
+#include "mongo/db/commands.h"
+
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
+
+
+namespace mongo {
+
+/**
+ * Example attachCollection command:
+ *   {
+ *       attachCollection: {
+ *          ns: "test.foo",
+ *          metadata: {
+ *              ...
+ *          },
+ *          numRecords: Long("1"),
+ *          dataSize: Long("46"),
+ *          collectionFile: '/data/path/collection-7--6037007956219898235.wt',
+ *          indexFiles: {
+ *              _id_: '/data/path/index-8--6037007956219898235.wt'
+ *          },
+ *          storageMetadata: {
+ *              'collection-7--6037007956219898235': {
+ *                  tableMetadata: ' ... ',
+ *                  fileMetadata: ' ... '
+ *              },
+ *              'index-8--6037007956219898235': {
+ *                  tableMetadata: ' ... ',
+ *                  fileMetadata: ' ... '
+ *              }
+ *          },
+ *          directoryPerDB: false,
+ *          directoryForIndexes: false
+ *       }
+ *   }
+ */
+class AttachCollectionCmd : public BasicCommand {
+public:
+    AttachCollectionCmd() : BasicCommand("attachCollection") {}
+
+    AllowedOnSecondary secondaryAllowed([[maybe_unused]] ServiceContext* context) const override {
+        return AllowedOnSecondary::kAlways;
+    }
+
+    bool supportsWriteConcern([[maybe_unused]] const BSONObj& cmd) const override {
+        return false;
+    }
+
+    Status checkAuthForOperation(OperationContext* opCtx,
+                                 const DatabaseName& dbName,
+                                 const BSONObj& cmdObj) const override {
+        auto* as = AuthorizationSession::get(opCtx->getClient());
+        if (!as->isAuthorizedForActionsOnResource(parseResourcePattern(dbName, cmdObj),
+                                                  ActionType::importCollection)) {
+            return {ErrorCodes::Unauthorized, "unauthorized"};
+        }
+
+        return Status::OK();
+    }
+
+    bool run(OperationContext* opCtx,
+             const DatabaseName& dbName,
+             const BSONObj& cmdObj,
+             BSONObjBuilder& result) override {
+        // TODO: jshdg
+        return true;
+    }
+
+} attachCollectionCmd;
+}  // namespace mongo
