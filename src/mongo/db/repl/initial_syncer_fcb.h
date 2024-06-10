b905f2ea08003951957b400ab44394f80cfe55dc
Igor Solodovnikov <igor.solodovnikov@percona.com>
2024-06-10 01:51:37 +0100

    PSMDB-1284 implement file cloning from the sync source

--- src/mongo/db/repl/initial_syncer_fcb.h
+++ src/mongo/db/repl/initial_syncer_fcb.h
@@ -32,6 +32,7 @@ Copyright (C) 2024-present Percona and/or its affiliates. All rights reserved.
 
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
 #include <memory>
 #include <string>
@@ -42,6 +43,7 @@ Copyright (C) 2024-present Percona and/or its affiliates. All rights reserved.
 #include "mongo/base/string_data.h"
 #include "mongo/bson/bsonobj.h"
 #include "mongo/bson/bsonobjbuilder.h"
+#include "mongo/client/dbclient_connection.h"
 #include "mongo/client/fetcher.h"
 #include "mongo/db/operation_context.h"
 #include "mongo/db/repl/callback_completion_guard.h"
@@ -61,6 +63,7 @@ Copyright (C) 2024-present Percona and/or its affiliates. All rights reserved.
 #include "mongo/util/duration.h"
 #include "mongo/util/net/hostandport.h"
 #include "mongo/util/time_support.h"
+#include "mongo/util/uuid.h"
 
 namespace mongo {
 namespace repl {
@@ -178,6 +181,14 @@ public:
     void setAllowedOutageDuration_forTest(Milliseconds allowedOutageDuration);
 
 private:
+    /**
+     * Attributes of remote file received from $backupCursor
+     */
+    struct BackupFile {
+        std::string name;
+        size_t size;
+    };
+
     /**
      * Returns true if we are still processing initial sync tasks (_state is either Running or
      * Shutdown).
@@ -321,6 +332,13 @@ private:
                                     std::shared_ptr<OnCompletionGuard> onCompletionGuard) noexcept;
 
     /**
+     * Callback to transfer file from the sync source
+     */
+    void _transferFileCallback(const executor::TaskExecutor::CallbackArgs& callbackArgs,
+                               std::size_t fileIdx,
+                               std::shared_ptr<OnCompletionGuard> onCompletionGuard) noexcept;
+
+    /**
      * This function does the following:
      *      1.) Truncate oplog.
      *      2.) Drop user databases (replicated dbs).
@@ -419,6 +437,7 @@ private:
 
     /**
      * Temporary location to declare all FCB-related private methods
+     * TODO: reorganize
      */
     Status _moveFiles(const std::vector<std::string>& files,
                       const std::string& sourceDir,
@@ -458,6 +477,9 @@ private:
     ThreadPool* _writerPool;                                           // (R)
     StorageInterface* _storage;                                        // (R)
     ReplicationProcess* _replicationProcess;                           // (S)
+    std::vector<BackupFile> _remoteFiles;                              // TODO:
+    UUID _backupId;                                                    // TODO:
+    std::string _remoteDBPath;                                         // TODO:
 
     // This is invoked with the final status of the initial sync. If startup() fails, this callback
     // is never invoked. The caller gets the last applied optime when the initial sync completes
@@ -475,8 +497,14 @@ private:
     // Handle to currently scheduled _fetchBackupCursorCallback() task.
     executor::TaskExecutor::CallbackHandle _fetchBackupCursorHandle;  // (M)
 
+    // Handle to currently scheduled _transferFileCallback() task.
+    executor::TaskExecutor::CallbackHandle _transferFileHandle;  // (M)
+
     // RollbackChecker to get rollback ID before and after each initial sync attempt.
     std::unique_ptr<RollbackChecker> _rollbackChecker;  // (M)
+
+    // Handle returned from RollbackChecker::reset().
+    RollbackChecker::CallbackHandle _getBaseRollbackIdHandle;  // (M)
 
     // Handle returned from RollbackChecker::checkForRollback().
     RollbackChecker::CallbackHandle _getLastRollbackIdHandle;  // (M)
