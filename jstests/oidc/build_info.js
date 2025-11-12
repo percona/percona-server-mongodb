d4fffed10d00a3cae720d872da88d5bf802489a2
Konstantin Trushin <konstantin.trushin@percona.com>
2025-11-12 18:10:07 +0400

    PSMDB-1845 Adjust feature reporting
    Instead of listing the features of discontinued Pro builds, list all
    the features Percona implements on top of MongoDB Community Edition.

--- jstests/oidc/build_info.js
+++ jstests/oidc/build_info.js
@@ -1,13 +1,14 @@
 import {OIDCFixture, ShardedCluster, StandaloneMongod} from 'jstests/oidc/lib/oidc_fixture.js';
 
-// Function to check if the OIDC feature is included in the build info's 'proFeatures' field.
+// Function to check if the OIDC feature is included in the build info's 'perconaFeatures' field.
 function checkBuildInfo(clusterClass) {
     const test = new OIDCFixture({oidcProviders: [], idps: []});
     test.setup(clusterClass);
 
     const buildInfo = assert.commandWorked(test.admin.runCommand({buildInfo: 1}));
     assert(buildInfo, "Failed to get build info");
-    assert(buildInfo.proFeatures.includes("OIDC"), "OIDC feature should be included in build info");
+    assert(buildInfo.perconaFeatures.includes("OIDC"),
+           "OIDC feature should be included in build info");
 
     test.teardown();
 }
