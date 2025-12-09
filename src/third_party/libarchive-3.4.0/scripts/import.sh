c995cf07359e409b1b5a537d1cf7733457ddd37e
Konstantin Trushin <konstantin.trushin@percona.com>
2025-12-09 14:53:03 +0400

    _WIP_ PSMDB-1867 Add libarchive to SBOM

---
+++ src/third_party/libarchive-3.4.0/scripts/import.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This script downloads and imports nlohmann-json.
+#
+# Turn on strict error checking, like perl use 'strict'
+set -xeuo pipefail
+IFS=$'\n\t'
+
+NAME="libarchive"
+
+VERSION="3.4.0"
+
+echo "This script is a temprorary stub"
+
