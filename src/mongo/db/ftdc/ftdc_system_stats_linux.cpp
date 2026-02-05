8a77adce623abc2e83ea73fd12839cf561d12fd7
Igor Solodovnikov <igor.solodovnikov@percona.com>
2026-02-05 17:48:29 +0000

    PSMDB-1893 Add server parameters to control 'disks' and 'mounts' subsections under systemMetrics in FTDC data
    By default both subsections are enabled (as it was before).
    
    Parameter names:
    - diagnosticDataCollectionEnableSystemMetricsDisks
    - diagnosticDataCollectionEnableSystemMetricsMounts
    
    Usage in mongo shell (at runtime):
    
        > db.adminCommand({setParameter: 1, diagnosticDataCollectionEnableSystemMetricsDisks: false})
        > db.adminCommand({setParameter: 1, diagnosticDataCollectionEnableSystemMetricsDisks: true})
        > db.adminCommand({setParameter: 1, diagnosticDataCollectionEnableSystemMetricsMounts: false})
        > db.adminCommand({setParameter: 1, diagnosticDataCollectionEnableSystemMetricsMounts: true})
    
    Usage in server config file (at startup):
    
        setParameter:
          diagnosticDataCollectionEnableSystemMetricsDisks: true
          diagnosticDataCollectionEnableSystemMetricsDisks: false
          diagnosticDataCollectionEnableSystemMetricsMounts: true
          diagnosticDataCollectionEnableSystemMetricsMounts: false
    
    Usage as command line parameters (at startup):
    
        --setParameter diagnosticDataCollectionEnableSystemMetricsDisks=false
        --setParameter diagnosticDataCollectionEnableSystemMetricsDisks=true
        --setParameter diagnosticDataCollectionEnableSystemMetricsMounts=false
        --setParameter diagnosticDataCollectionEnableSystemMetricsMounts=true

--- src/mongo/db/ftdc/ftdc_system_stats_linux.cpp
+++ src/mongo/db/ftdc/ftdc_system_stats_linux.cpp
@@ -40,6 +40,7 @@
 #include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/ftdc/collector.h"
 #include "mongo/db/ftdc/controller.h"
+#include "mongo/db/ftdc/ftdc_server_gen.h"
 #include "mongo/db/ftdc/ftdc_system_stats.h"
 #include "mongo/util/errno_util.h"
 #include "mongo/util/functional.h"
@@ -169,7 +170,7 @@ public:
 
         // Skip the disks section if we could not find any disks.
         // This can happen when we do not have permission to /sys/block for instance.
-        if (!_disksStringData.empty()) {
+        if (gDiagnosticDataCollectionEnableSystemMetricsDisks.load() && !_disksStringData.empty()) {
             BSONObjBuilder subObjBuilder(builder.subobjStart("disks"_sd));
             processStatusErrors(procparser::parseProcDiskStatsFile(
                                     "/proc/diskstats"_sd, _disksStringData, &subObjBuilder),
@@ -177,7 +178,7 @@ public:
             subObjBuilder.doneFast();
         }
 
-        {
+        if (gDiagnosticDataCollectionEnableSystemMetricsMounts.load()) {
             BSONObjBuilder subObjBuilder(builder.subobjStart("mounts"_sd));
             processStatusErrors(
                 procparser::parseProcSelfMountStatsFile("/proc/self/mountinfo"_sd, &subObjBuilder),
