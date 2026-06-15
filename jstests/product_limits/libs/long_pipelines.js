4ef1853803f85c7b00e1fb62239481cd0763b6d3
Konstantin Trushin <konstantin.trushin@percona.com>
2026-06-15 16:38:23 +0400

    PSMDB-1990 Avoid `mongod` crash in tests (#1912)
    Running the `DatasetManyCollections.WorkloadManyCollectionsLookupBushy`
    test from the `product_limits` suite resulted in a `mongod` crash due
    to a stack overflow while processing a long aggregation pipeline.
    
    Since the issue does not reproduce in the `master` branch at the time
    of writing (commit `3d02afa7e1c`), we expect upstream to backport the
    fix into branch `v8.3`. We don't want to interfere with upstream by
    devising our own fix or doing the backport ourselves, in order to
    minimize the potential for future merge conflicts. Instead, we
    temporarily halve the pipeline length so that the stack overflow does
    not happen.
    
    After upstream has fixed branch `v8.3`, this commit can be reverted.

--- jstests/product_limits/libs/long_pipelines.js
+++ jstests/product_limits/libs/long_pipelines.js
@@ -42,7 +42,10 @@ export class WorkloadManyCollectionsInLookupBushy extends LongPipelineWorkload {
         // Too many $lookups result in "errmsg" : "BSONObj size: 53097740 (0x32A350C) is invalid.
         // Size must be between 0 and 16793600(16MB) First element: slots: \"$$RESULT=s7202 env: {
         // }\"",
-        return Math.min(500, super.scale());
+        //
+        // TODO: revert the value back to 500 after the stack overflow has been
+        // fixed. For more details, please see the commit message.
+        return Math.min(250, super.scale());
     }
 
     pipeline() {
