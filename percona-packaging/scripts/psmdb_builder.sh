892b648f8599a1a955eb8ef41bc68fb6612564b5
Sandra <sandra.romanchenko@percona.com>
2026-07-29 11:42:57 +0300

    PSMDB. Update mongo tools deps to prevent CVE-2026-56852

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -273,8 +273,8 @@ get_sources(){
         sed -i '/GetLinuxDistroAndVersion()/ s/os, version, err = GetLinuxDistroAndVersion()/os, version, err = "rhel", "9.3", nil/' release/platform/platform.go || abort '`sed` failed'
     #fi
 
-    go mod edit -dropreplace golang.org/x/crypto@v0.45.0 -dropreplace golang.org/x/net@v0.47.0 || abort '`go mod edit` failed'
-    go get golang.org/x/crypto@v0.52.0 golang.org/x/net@v0.55.0 || abort '`go get` failed'
+    go mod edit -dropreplace golang.org/x/crypto@v0.45.0 -dropreplace golang.org/x/net@v0.47.0 -dropreplace golang.org/x/text@v0.31.0 || abort '`go mod edit` failed'
+    go get golang.org/x/crypto@v0.52.0 golang.org/x/net@v0.55.0 golang.org/x/text@v0.39.0 || abort '`go get` failed'
     go mod tidy || abort '`go mod tidy` failed'
     go mod vendor || abort '`go mod vendor` failed'
     # Make downloaded Go packages writable to be able to remove them later
