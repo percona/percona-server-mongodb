03c0c74a157775c3eb1b5b71d5c61dea8cf68793
Sandra <sandra.romanchenko@percona.com>
2026-09-15 12:07:13 +0300

    PSMDB-2261. Update go and libs to fix CVEs

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -274,7 +274,7 @@ get_sources(){
     #fi
 
     go mod edit -dropreplace golang.org/x/crypto@v0.45.0 -dropreplace golang.org/x/net@v0.47.0 -dropreplace golang.org/x/text@v0.31.0 -dropreplace github.com/klauspost/compress@v1.17.8 || abort '`go mod edit` failed'
-    go get golang.org/x/crypto@v0.53.0 golang.org/x/net@v0.56.0 golang.org/x/text@v0.39.0 github.com/klauspost/compress@v1.18.7 || abort '`go get` failed'
+    go get golang.org/x/crypto@v0.56.0 golang.org/x/net@v0.57.0 golang.org/x/text@v0.41.0 github.com/klauspost/compress@v1.18.7 || abort '`go get` failed'
     go mod tidy || abort '`go mod tidy` failed'
     go mod vendor || abort '`go mod vendor` failed'
     # Make downloaded Go packages writable to be able to remove them later
@@ -377,7 +377,7 @@ install_golang() {
         abort "Unsupported architecture: $ARCH"
     fi
 
-    GO_VERSION="1.26.6"
+    GO_VERSION="1.26.8"
     GO_TAR="go${GO_VERSION}.linux-${GO_ARCH}.tar.gz"
     GO_SHA="${GO_TAR}.sha256"
     GO_URL="https://downloads.percona.com/downloads/packaging/go/${GO_TAR}"
